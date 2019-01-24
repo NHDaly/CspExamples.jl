8c930de05b6044a9a9e4d69b8257b04faedbb9dd
Nathan Daly <nhdaly@gmail.com>
2019-01-24 00:53:22 -0500

    Add first three examples: COPY, SQUASH, SQUASH_EXT
    Adds src and tests for those three functions. I copied the docstrings
    format from thomas11's example as well.

--- src/CspExamples.jl
+++ src/CspExamples.jl
@@ -1,4 +1,75 @@
 module CspExamples
 
+"""
+    S31_COPY(west::Channel, east::Channel)
+
+3.1 COPY
+
+> "Problem: Write a process X to copy characters output by process west
+to process, east."
+
+As an addition to the paper's example, we stop when west is closed,
+otherwise we would just hang at this point. To indicate this to the
+client, we close the east channel.
+"""
+function S31_COPY(west::Channel{Char}, east::Channel{Char})
+    for c in west
+        put!(east, c)
+    end
+    close(east)
+end
+
+"""
+    S32_SQUASH(west::Channel, east::Channel)
+
+3.2 SQUASH
+
+> "Problem: Adapt the previous program [COPY] to replace every pair of
+consecutive asterisks "**" by an upward arrow "↑". Assume that the final
+character input is not an asterisk."
+"""
+function S32_SQUASH(west::Channel{Char}, east::Channel{Char})
+    for c in west
+        if c == '*'
+            c1 = take!(west)
+            if c1 == '*'
+                put!(east, '↑')
+            else
+                put!(east, c)
+                put!(east, c1)
+            end
+        else
+            put!(east, c)
+        end
+    end
+    close(east)
+end
+
+"""
+    S32_SQUASH_EXT(west::Channel, east::Channel)
+
+Hoare adds a remark to 3.2 SQUASH: "(2) As an exercise, adapt this
+process to deal sensibly with input which ends with an odd number of
+asterisks."
+"""
+function S31_SQUASH_EXT(west::Channel{Char}, east::Channel{Char})
+    for c in west
+        if c == '*'
+            # TODO: Is it possible to also have a timeout like thomas11 does in Go?
+            try
+                c1 = take!(west)
+                if c1 == '*'
+                    put!(east, '↑')
+                else
+                    put!(east, c)
+                    put!(east, c1)
+                end
+                continue
+            catch end
+        end
+        put!(east, c)
+    end
+    close(east)
+end
 
 end
--- test/CspExamples.jl
+++ test/CspExamples.jl
@@ -1,2 +1,44 @@
 include("../src/CspExamples.jl")
 using Test
+
+fill_channel(ch, input) = for v in input
+    put!(ch, v)
+end
+
+@testset "S31_COPY" begin
+    input = "hello"
+    # The input channel is a 1-character buffer as described in the paper.
+    west = Channel(ch->(fill_channel(ch, input)), csize=1, ctype=Char)
+    east = Channel{Char}(Inf) # allow buffering the output infinitely for ease of testing
+    CspExamples.S31_COPY(west, east)
+    @test String(collect(east)) == input
+end
+
+@testset "S32_SQUASH" begin
+    # The input channel is a 1-character buffer as described in the paper.
+    west = Channel(csize=1, ctype=Char) do ch
+        fill_channel(ch, "hello*to**the*world")
+    end
+    east = Channel{Char}(Inf) # allow buffering the output infinitely for ease of testing
+    CspExamples.S32_SQUASH(west, east)
+    @test String(collect(east)) == "hello*to↑the*world"
+end
+
+# Convenience function to create and fill a channel.
+make_filled_channel(input; csize=0, ctype=eltype(input)) = Channel(csize=csize, ctype=ctype) do ch
+    fill_channel(ch, input)
+end
+
+@testset "S32_SQUASH_EXT" begin
+    # Test 1 * at end
+    west = make_filled_channel("hello**world*", csize=1)
+    east = Channel{Char}(Inf)
+    CspExamples.S32_SQUASH_EXT(west, east)
+    @test String(collect(east)) == "hello↑world*"
+
+    # Test 3 *s at end
+    west = make_filled_channel("hello**world***", csize=1)
+    east = Channel{Char}(Inf)
+    CspExamples.S32_SQUASH_EXT(west, east)
+    @test String(collect(east)) == "hello↑world↑*"
+end
